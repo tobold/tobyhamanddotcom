346cafecd4100f09f046842b8db40420bac7496e
Toby Hamand <work@tobyhamand.com>
2018-11-19 22:19:37 +0000

    installed gatsby + typescript + sass

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    name: `tobyhamanddotcom`,
+    tagline: `if i were a website.`
+  },  
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-tslint`,
+  ],
+}
