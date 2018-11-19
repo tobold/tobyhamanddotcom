4a24d3c9796190ac34cf77d008ab1d284915602f
Toby Hamand <work@tobyhamand.com>
2018-11-19 22:51:22 +0000

    basic headshot index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  siteMetadata: {
-    name: `tobyhamanddotcom`,
-    tagline: `if i were a website.`
-  },  
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,39 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import {graphql} from 'gatsby';
-
-interface IndexPageProps {
-  data: {
-    site: {
-      siteMetadata: {
-        name: string;
-        tagline: string;
-      },
-    },
-  };
-}
-
-export const indexPageQuery = graphql`
-  query IndexPageQuery {
-    site {
-      siteMetadata {
-        name
-        tagline
-      }
-    }
-  }
-`;
+// @ts-ignore
+import headshot from '../../public/images/toby.png';
 
 const Container = styled.div`
   display: flex;
-  align-content: center;
+  align-items: center;
+  flex-direction: column;
+  max-width: 60rem;
+  margin: 3rem auto 0;
+  font-family: sans-serif;
+
+  h1 {
+    color: #333;
+  }
+
+  p {
+    color: #aaa;
+  }
 `;
 
-export default class IndexPage extends React.Component<IndexPageProps, {}> {
+const Headshot = styled.img`
+  width: 300px;
+  height: 300px;
+  border-radius: 150px;
+`;
+
+export default class IndexPage extends React.Component<{}, {}> {
 
   public render() {
-    const {
-      name,
-      tagline,
-    } = this.props.data.site.siteMetadata;
-
     return (
       <Container>
-        <h1>{name}</h1>
-        <p>{tagline}</p>
+        <Headshot src={headshot}/>
+        <h1>Hello.</h1>
+        <p>if i were a website.</p>
       </Container>
     );
   }
