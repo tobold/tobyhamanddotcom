8bf99020377a52ce351db2b3f6e4a2359ea52332
Toby Hamand <work@tobyhamand.com>
2018-12-28 15:34:58 +0100

    extracted icons and theme

---
+++ src/components/SocialIcon/SocialLinks.ts
@@ -0,0 +1,8 @@
+export const LinkedinUrl = 'https://www.linkedin.com/in/toby-hamand/';
+export const StackoverflowUrl = 'https://stackoverflow.com/users/8558260/toby-hamand';
+export const GithubUrl = 'https://github.com/tobold';
+export const InstagramUrl = 'http://instagram.com/tobyhamand';
+export const LastfmUrl = 'https://www.last.fm/user/Tobold';
+export const FacebookUrl = 'https://facebook.com/tobold';
+export const TwitterUrl = 'https://twitter.com/tobyhamand';
+export const EmailUrl = '"mailto:work@tobyhamand.com"';
---
+++ src/theme.ts
@@ -0,0 +1,24 @@
+const colors = {
+  black: '#000000',
+  black10: '#191919',
+  white10: '#e5e5e5',
+  white: '#ffffff',
+  pink: 'hotpink',
+  green: 'green',
+};
+
+export const lightTheme = {
+  primary: colors.black,
+  primaryHover: colors.black10,
+  secondary: colors.white,
+  secondaryHover: colors.white10,
+  background: colors.white10,
+};
+
+export const darkTheme = {
+  primary: colors.white,
+  primaryHover: colors.white10,
+  secondary: colors.black,
+  secondaryHover: colors.black10,
+  background: colors.black10,
+};
